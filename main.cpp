ae572e1dfab271825072a90e4d79295f4d32a2b7
David <David15.8Krug@web.de>
2021-05-13 19:33:42 +0200

    Add main

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "bst.hpp"
+
+/*
+Pseudocode printBST(node X) {
+	if node[left] == existant:
+		nodeX -> node[left];		//Verbindung zw node X und linkem Kindknoten
+		printBST(node[left]);		//Aufruf für linkes Kind von X (und all deren Kinder)
+	if node[right] == existant:
+		nodeX -> node[right];		//Verbindung zw node X und linkem Kindknoten
+		printBST(node[right]);		//Rekursiver Aufruf
+}
+*/
+
+int main()
+{
+  //printBST()
+  return 1;
+}
