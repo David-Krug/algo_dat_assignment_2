5956eb0d6f0079cce52c079d873ef792a43ceff7
David <David15.8Krug@web.de>
2021-05-13 19:34:06 +0200

    Add basic data structure of binary search tree

---
+++ bst.cpp
@@ -0,0 +1,39 @@
+#include "bst.hpp"
+
+//Bst
+int* Bst::search(int key_value) const
+{
+  
+}
+
+int* Bst::minimum() const
+{
+  
+}
+
+int* Bst::maximum() const
+{
+  
+}
+
+int* Bst::successor(Bst_Node const& node) const
+{
+  
+}
+
+int* Bst::predecessor(Bst_Node const& node) const
+{
+  
+}
+
+void Bst::add_node(Bst_Node const& node)
+{
+  
+}
+
+void Bst::remove_node(Bst_Node const& node)
+{
+  
+}
+
+//Bst_Node
--- bst.hpp
+++ bst.hpp
@@ -1,10 +1,25 @@
-/*
-Pseudocode printBST(node X) {
-	if node[left] == existant:
-		nodeX -> node[left];		//Verbindung zw node X und linkem Kindknoten
-		printBST(node[left]);		//Aufruf für linkes Kind von X (und all deren Kinder)
-	if node[right] == existant:
-		nodeX -> node[right];		//Verbindung zw node X und linkem Kindknoten
-		printBST(node[right]);		//Rekursiver Aufruf
+#ifndef BST_HPP
+#define BST_HPP
+
+struct Bst()
+{
+  //member functions
+  int* search(int key_value) const;
+  int* minimum() const;
+  int* maximum() const;
+  int* successor(Bst_Node const& node) const;
+  int* predecessor(Bst_Node const& node) const;
+  void add_node(Bst_Node const& node);
+  void remove_node(Bst_Node const& node);
 }
-*/
+
+template <typename T>
+struct Bst_Node()
+{
+  //member variables
+  T data_;
+  struct Bst_Node* left_child_;
+  struct Bst_Node* right_child_;
+}
+
+#endif // !BST_HPP
