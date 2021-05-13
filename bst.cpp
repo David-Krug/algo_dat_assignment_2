983654c4ee5dfd28fedc776f0070dc56b3179608
ViridaAlice <virida.a@gmail.com>
2021-05-13 23:56:45 +0200

    Function Comments & Pseudocode

--- bst.cpp
+++ bst.cpp
@@ -1,39 +1,90 @@
 #include "bst.hpp"
 
 //Bst
-int* Bst::search(int key_value) const
+int* Bst::search(int key_value) const		//Should return pointer to node (?) 
 {
-  
+	//if nodeX.value == key_value, return (pointer to) node X
+	
+	//else if nodeX.value > key_value, check nodeX.leftchild,
+		//if NIL, then key_value doesn't exist within tree
+		//if not NIL, jump back to first if (recursive function?)
+
+	//else if nodeX.value < key_value, check nodeX.rightchild, 
+		//if NIL, then key_value doesn't exist within tree
+		//if not NIL, jump back to first if (recursive function?)
 }
 
-int* Bst::minimum() const
+int* Bst::minimum() const				//Needs node as parameter to know where to start (important for successor & predecessor functions)
 {
-  
+	//while nodeX.leftchild != NIL:
+		//nodeX = nodeX.leftchild
+		
+	//return nodeX or nodeX.value;
 }
 
-int* Bst::maximum() const
+int* Bst::maximum() const				//Needs node as parameter to know where to start (important for successor & predecessor functions)
 {
-  
+	//while nodeX.rightchild != NIL:
+		//nodeX = nodeX.rightchild
+		
+	//return nodeX or nodeX.value;
 }
 
 int* Bst::successor(Bst_Node const& node) const
 {
-  
+	//if nodeX.rightchild != NIL:
+		//get minimum of right subtree
+
+	//else:
+		//(find first ancestor reached from the left)
+		//check if NodeX is parent's left child:
+			//if true, then parent is successor
+			//if false, go further up
 }
 
 int* Bst::predecessor(Bst_Node const& node) const
 {
-  
+	//if nodeX.leftchild != NIL:
+		  //get maximum of left subtree
+
+	//else:
+		//find first ancestor reached from the right
+		//check if NodeX is parent's right child:
+			//if true, then parent is successor
+			//if false, go further up
 }
 
 void Bst::add_node(Bst_Node const& node)
 {
-  
+	//while nodeX.value != NIL:
+		//if nodeX.value > node.value:
+			//nodeX = nodeX.leftchild;
+		//if nodeX.value < node.value:
+			//nodeX = nodeX.rightchild;
+
+	//insert node at place nodeX
+	//(after while-loop, nodeX is empty child of a node)
 }
 
 void Bst::remove_node(Bst_Node const& node)
 {
-  
+	//if node.leftchild == NIL and node.rightchild == NIL:
+		//replace node with NIL
+
+	//if (node.leftchild != NIL and node.leftchild == NIL) OR (node.leftchild == NIL and node.leftchild != NIL):
+		//check if node's child has children and so forth
+		//if not: 
+			//replace node with node's child 
+			//replace node's child with NIL
+
+	//if node.leftchild != NIL and node.rightchild != NIL:
+		//if node.rightchild.leftchild == NIL:
+			//replace node with node.rightchild
+			//replace node.rightchild with NIL
+		//else:
+			//find and store successor nodeY of node
+			//remove_node(nodeY)
+			//replace node with nodeY's value
 }
 
 //Bst_Node
--- main.cpp
+++ main.cpp
@@ -1,13 +1,47 @@
 #include "bst.hpp"
 
 /*
-Pseudocode printBST(node X) {
-	if node[left] == existant:
-		nodeX -> node[left];		//Verbindung zw node X und linkem Kindknoten
-		printBST(node[left]);		//Aufruf für linkes Kind von X (und all deren Kinder)
-	if node[right] == existant:
-		nodeX -> node[right];		//Verbindung zw node X und linkem Kindknoten
-		printBST(node[right]);		//Rekursiver Aufruf
+void printBST (Bst_Node const& node, FILE* file) {
+    fprintf(file, "digraph BST { \n");
+    fprintf(file, "    node [fontname=\"Arial\"]; \n");
+    fprintf(file, "    graph [ordering="out"]; \n");
+
+    if (node.value == NIL) {
+        fprintf(file, "\n");
+    }
+    else if (node.rightchild == NIL && node.leftchild == NIL) {
+        fprintf(stream, "    %d; \n", node.value);
+    }
+    else {
+        print_node(node, file);
+    }
+    fprintf(file, "}");
+}
+
+void print_node (Bst_Node const& node, FILE* file) {
+    static int nil = 0;
+
+    if (node.leftchild != NIL) {
+        fprintf(file, "    %d -> %d; \n", node.value, node.leftchild.value);
+        print_node(node.leftchild, file);
+    }
+    else {
+        print_null(node.value, nil++, file);
+    }
+
+    if (node.rightchild != NIL) {
+        fprintf(file, "    %d -> %d; \n", node.value, node.rightchild.value);
+        print_node(node.rightchild, file);
+    }
+    else {
+        print_null(node.value, nil++, file);
+    }
+}
+
+void print_nil(int key, int nilcount, FILE* file)
+{
+    fprintf(file, "    nil%d [shape=point]; \n", nilcount);
+    fprintf(file, "    %d -> nil%d; \n", key, nilcount);
 }
 */
 
